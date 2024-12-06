50455e4df1f50c9440c566feb002cbec940384df
B0UGDX <77412296+victoMR@users.noreply.github.com>
2024-12-06 17:47:24 -0600

    Implementando cors

--- blockchain/wsgi.py
+++ blockchain/wsgi.py
@@ -1,4 +1,9 @@
 from main import app
 
-if __name__ == "__main__":
-    app.run()
+##Implementando cors para https://test-si-pi.vercel.app/
+
+from flask_cors import CORS
+CORS(app, resources={r"/*": {"origins": "https://test-si-pi.vercel.app"}})
+
+if __name__ == '__main__':
+    app.run(host='https://testsi.onrender.com/', port=5000)
