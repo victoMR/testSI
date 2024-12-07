c4cfe9a595471b59e6ad56427ec8cf5193d696a6
B0UGDX <77412296+victoMR@users.noreply.github.com>
2024-12-06 18:57:22 -0600

    fix commit v2

--- deploy/wsgi.py
+++ deploy/wsgi.py
@@ -6,4 +6,4 @@ from flask_cors import CORS
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == '__main__':
-    app.run(host='https://testsi-1.onrender.com/', port=5000)
+    app.run(host='0.0.0.0', port=5000)
